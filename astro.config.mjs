dc9f9a49c756ff3666143eddbb8be93a995b6094
jose luis <mamanijoss635@gmail.com>
2026-06-25 17:19:39 -0500

    agregando ultimo retoce

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel'; // 1. IMPORTAMOS EL ADAPTADOR QUE INSTALASTE
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // 2. LE DECIMOS A ASTRO QUE TRABAJE EN MODO SERVIDOR PARA LEER SUPABASE EN VIVO
+  output: 'server',
+  
+  // 3. LE PASAMOS EL ADAPTADOR OFICIAL PARA VERCEL
+  adapter: vercel(),
+});
